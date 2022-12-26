7b6137fac73f317242f81eac5688f0822acdfb64
fsanche3 <fsanche3@oswego.edu>
2022-12-26 11:07:15 -0500

    made progress on selected meal stage component & need to change the template of design

--- ng-front-end/src/app/components/feed/feed.component.ts
+++ ng-front-end/src/app/components/feed/feed.component.ts
@@ -17,6 +17,18 @@ export class FeedComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  goToStage(){
+    sessionStorage.setItem("name", this.food.name);
+    sessionStorage.setItem("description", this.food.description);
+    sessionStorage.setItem("type", this.food.type);
+    sessionStorage.setItem("cook_time", this.food.cook_time);
+    sessionStorage.setItem("img_url", this.food.img_url);
+    sessionStorage.setItem("instructions", this.food.instructions);
+    sessionStorage.setItem("chef_id", this.food.chef_id);
+    sessionStorage.setItem("looks", this.food.looks);
+    this.router.navigate(['stage']);
+
+  }
 
 
 }
--- ng-front-end/src/app/components/stage/stage.component.ts
+++ ng-front-end/src/app/components/stage/stage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stage',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StageComponent implements OnInit {
 
-  constructor() { }
+  mealSelected: boolean = false;
+  name: any;
+  description: any;
+  type: any;
+  cook_time: any;
+  img_url: any;
+  instructions: any;
+  chef_id: any;
+  looks: any;
+  ingredients: any;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if(sessionStorage.getItem('name') == ''){
+      this.router.navigate(['home']);
+    }
+    this.setUp();
+  
   }
+
+  setUp(){
+    this.mealSelected = !this.mealSelected;
+    this.name = sessionStorage.getItem('name');
+    this.description = sessionStorage.getItem('description');
+    this.type = sessionStorage.getItem('type');
+    this.cook_time = sessionStorage.getItem('cook_time');
+    this.img_url = sessionStorage.getItem('img_url');
+    this.instructions = sessionStorage.getItem('instructions');
+    this.chef_id = sessionStorage.getItem('chef_id');
+    this.looks = sessionStorage.getItem('looks');
+    this.ingredients = sessionStorage.getItem('ingredients');
+
+  }
+
 
 }
