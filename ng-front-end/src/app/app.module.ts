84dce3af1fb114881e6679cdf31a00395fe537fd
fsanche3 <fsanche3@oswego.edu>
2022-12-26 21:39:39 -0500

    finished template for stage page need to finsih modal pop up

--- ng-front-end/src/app/app.module.ts
+++ ng-front-end/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { NavComponent } from './components/nav/nav.component';
 import { FeedComponent } from './components/feed/feed.component';
 import { HomeComponent } from './components/home/home.component';
 import { StageComponent } from './components/stage/stage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 
 
@@ -16,10 +19,12 @@ import { StageComponent } from './components/stage/stage.component';
     FeedComponent,
     HomeComponent,
     StageComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng-front-end/src/app/components/home/home.component.ts
+++ ng-front-end/src/app/components/home/home.component.ts
@@ -22,7 +22,7 @@ anyEggs = {
   'type': 'Breeky',
   'cook_time': '60min',
   'img_url': "https://www.thespruceeats.com/thmb/MCGZ1A0-1tkXvdjt8G50gcTZPV0=/1855x1288/filters:fill(auto,1)/steak-and-eggs-4780185-07-0f49f22e6fc44a0c8df04f646ac81d0d.jpg",
-  'intsructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing",
+  'instructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing",
   'chef_id': "2"
 };
 
@@ -33,7 +33,7 @@ yummyEggs = {
   'type': 'Snackums',
   'cook_time': '1min',
   'img_url': 'https://www.bing.com/th?id=OSK.d61106b295285a7af5ad07499ba7616f&w=218&h=327&rs=2&qlt=80&o=6&cdv=1&pid=16.1',
-  'intsructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing",
+  'instructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing",
   'chef_id': "2"
 };
 
@@ -44,11 +44,44 @@ garlicBread = {
   'type': 'Breeky',
   'cook_time': '1min',
   'img_url': 'https://th.bing.com/th/id/R.08d2fb4d727c58095d5fc9a86e4231d8?rik=DIhMjYXixikbyg&pid=ImgRaw&r=0',
-  'intsructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing",
+  'instructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing",
   'chef_id': "2"
 };
 
-list: any[] = [this.anyEggs, this.yummyEggs, this.garlicBread];
+platano = {
+  'id': '1',
+  'name': 'Fried water',
+  'description': "Air fried water",
+  'type': 'Lunch',
+  'cook_time': '99 Min',
+  'img_url': 'https://sm.mashable.com/mashable_in/seo/2/25824/25824_jzqs.jpg',
+  'instructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing",
+  'chef_id': "2"
+};
+
+salami = {
+  'id': '1',
+  'name': 'Fried water',
+  'description': "Air fried water",
+  'type': 'Lunch',
+  'cook_time': '99 Min',
+  'img_url': 'https://sm.mashable.com/mashable_in/seo/2/25824/25824_jzqs.jpg',
+  'instructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing",
+  'chef_id': "2"
+};
+
+huevo = {
+  'id': '1',
+  'name': 'Fried water',
+  'description': "Air fried water",
+  'type': 'Lunch',
+  'cook_time': '99 Min',
+  'img_url': 'https://sm.mashable.com/mashable_in/seo/2/25824/25824_jzqs.jpg',
+  'instructions': "YOO, i loved when i made steak instead of eggs instead, im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing im feeling like a chicken wing",
+  'chef_id': "2"
+};
+
+list: any[] = [this.anyEggs, this.yummyEggs, this.garlicBread, this.platano, this.salami, this.huevo];
 
   ngOnInit(): void {
   }
